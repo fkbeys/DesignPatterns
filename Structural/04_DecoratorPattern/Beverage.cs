2f65b630999ed759832fce25f59c2976b5ef4bbb
fkbeys <fkbeys@gmail.com>
2023-06-09 23:09:12 +0200

    Implementation of decorator pattern

---
+++ Structural/04_DecoratorPattern/Beverage.cs
@@ -0,0 +1,44 @@
+﻿namespace _04_DecoratorPattern
+{
+    public abstract class Beverage
+    {
+        public abstract string Description { get; }
+        public abstract decimal Price();
+    }
+
+    public class Espresso : Beverage
+    {
+        public override string Description => "Espresso";
+
+        public override decimal Price()
+        {
+            return 5;
+        }
+    }
+
+    public abstract class BeverageDecorator : Beverage
+    {
+        public readonly Beverage _beverage;
+
+        public BeverageDecorator(Beverage beverage)
+        {
+            this._beverage = beverage;
+        }
+    }
+
+    public class Milk : BeverageDecorator
+    {
+        public Milk(Beverage beverage) : base(beverage)
+        {
+        }
+
+        public override string Description => _beverage.Description + " With Milk";
+
+        public override decimal Price()
+        {
+            return _beverage.Price() + 1;
+        }
+    }
+
+
+}
---
+++ Structural/04_DecoratorPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04_DecoratorPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Espresso espresso = new Espresso();
+            BeverageDecorator decorator = new Milk(espresso);
+
+
+            Console.WriteLine("Name:" + decorator.Description + "Price: " + decorator.Price());
+
+            Console.ReadLine();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
