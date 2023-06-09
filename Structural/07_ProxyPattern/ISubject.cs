82217859ca089f822c2320d02993ef459aba0092
fkbeys <fkbeys@gmail.com>
2023-06-10 00:42:15 +0200

    Proxy Pattern implemented

---
+++ Structural/07_ProxyPattern/ISubject.cs
@@ -0,0 +1,38 @@
+﻿namespace _07_ProxyPattern
+{
+    public interface ISubject
+    {
+        void Request();
+
+    }
+
+    public class Subject : ISubject
+    {
+        public void Request()
+        {
+            Console.WriteLine("Subject requested...");
+        }
+    }
+
+    public class ProxyClass : ISubject
+    {
+        private Subject subject;
+
+        public ProxyClass(Subject subject)
+        {
+            this.subject = subject;
+        }
+        public void Request()
+        {
+            if (subject == null)
+            {
+                Console.WriteLine("subject is null,we need to create an instance");
+                subject = new Subject();
+            }
+            Console.WriteLine("Proxy request method worked...");
+            subject.Request();
+        }
+    }
+
+
+}
---
+++ Structural/07_ProxyPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07_ProxyPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ProxyClass proxyClass = new ProxyClass(null);
+            //proxyClass.Request();
+
+
+            var subject = new Subject();
+
+            ProxyClass proxyClass = new ProxyClass(subject);
+            proxyClass.Request();
+
+
+            Console.ReadLine();
+        }
+    }
+}
