8f60959c4edf7411113efcdd7bb199cd50857711
fkbeys <fkbeys@gmail.com>
2023-06-09 18:24:35 +0200

    Adapter pattern implemented

---
+++ Structural/01_AdapterPattern/Models/IPayment.cs
@@ -0,0 +1,65 @@
+﻿namespace _01_AdapterPattern.Models
+{
+    public interface IPayment
+    {
+        public bool MakePayment(int amount);
+    }
+
+    public class PayPal : IPayment
+    {
+        public bool MakePayment(int amount)
+        {
+            Console.WriteLine("Paypal Payment okey...");
+            return true;
+        }
+    }
+    public class Klarna
+    {
+        public bool Charge(int amount)
+        {
+            Console.WriteLine("Klarna payment okey...");
+            return true;
+        }
+    }
+
+    public class KlarnaAdapter : IPayment
+    {
+        private readonly Klarna _klarna;
+
+        public KlarnaAdapter(Klarna klarna)
+        {
+            this._klarna = klarna;
+        }
+
+        public bool MakePayment(int amount)
+        {
+            return _klarna.Charge(amount);
+        }
+    }
+
+    public class ShoppingOperations
+    {
+        private readonly IPayment payment;
+
+        public ShoppingOperations(IPayment payment)
+        {
+            this.payment = payment;
+        }
+
+        public void CheckOut(int amount)
+        {
+            var result = payment.MakePayment(amount);
+            if (result)
+            {
+                Console.WriteLine("Payment Ok");
+            }
+            else
+            {
+                Console.WriteLine("Payment Error");
+            } 
+        }
+
+    }
+
+
+}
---
+++ Structural/01_AdapterPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using _01_AdapterPattern.Models;
+
+namespace _01_AdapterPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingOperations shoppingOperations = new ShoppingOperations(new PayPal {  });
+            shoppingOperations.CheckOut(100);
+            
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
