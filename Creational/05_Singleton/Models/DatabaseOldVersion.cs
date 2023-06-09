2bab885d70e75f340089a1bbc9c1d3053e009078
fkbeys <fkbeys@gmail.com>
2023-06-09 16:12:29 +0200

    singleton old and new examples

---
+++ Creational/05_Singleton/Models/DatabaseOldVersion.cs
@@ -0,0 +1,33 @@
+﻿namespace _05_Singleton.Models
+{
+    public class DatabaseOldVersion
+    {
+        private static DatabaseOldVersion _database;
+        private static readonly object _lock = new object();
+
+        private DatabaseOldVersion()
+        {
+
+        }
+
+        public static DatabaseOldVersion database
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_database == null)
+                    {
+                        Console.WriteLine("db old singleton version created.");
+                        _database = new DatabaseOldVersion { };
+                    }
+                    return _database;
+                }
+            }
+        }
+    }
+}
+
+
+
+
--- Creational/05_Singleton/Program.cs
+++ Creational/05_Singleton/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace _05_Singleton
+﻿using _05_Singleton.Models;
+
+namespace _05_Singleton
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var fx1 = DatabaseOldVersion.database;
+            var fx2 = DatabaseOldVersion.database;
+            var fx3 = DatabaseOldVersion.database;
+            var fx4 = DatabaseOldVersion.database;
+            var fx5 = DatabaseOldVersion.database;
+            var fx6 = DatabaseOldVersion.database;
+
+
+            var lazy1 = Database.Instance;
+            var lazy2 = Database.Instance;
+            var lazy3 = Database.Instance;
+
+            Console.Read();
+
             Console.WriteLine("Hello, World!");
         }
     }
