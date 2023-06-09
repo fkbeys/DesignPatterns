c248d0ceb4f27f3c4585eb8e96ac7f185fa966f4
fkbeys <fkbeys@gmail.com>
2023-06-09 23:37:53 +0200

    Facade Pattern

---
+++ Structural/05_FacadePattern/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _05_FacadePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SubsystemA subsystemA = new();
+            SubsystemB subsystemB = new();
+            SubsystemManagement subsystemManagement = new(subsystemA, subsystemB);
+            Console.WriteLine(subsystemManagement.Work1());
+            Console.WriteLine(subsystemManagement.Work2());
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Structural/05_FacadePattern/SubsystemManagement.cs
@@ -0,0 +1,53 @@
+﻿namespace _05_FacadePattern
+{
+
+    public class SubsystemA
+    {
+        public string Work1()
+        {
+            return "A-1";
+        }
+
+        public string Work2()
+        {
+            return "A-2";
+        }
+    }
+
+    public class SubsystemB
+    {
+        public string Work1()
+        {
+            return "B-1";
+        }
+
+        public string Work2()
+        {
+            return "B-2";
+        }
+    }
+
+    public class SubsystemManagement
+    {
+        private readonly SubsystemA subsystemA;
+        private readonly SubsystemB subsystemB;
+
+        public SubsystemManagement(SubsystemA subsystemA, SubsystemB subsystemB)
+        {
+            this.subsystemA = subsystemA;
+            this.subsystemB = subsystemB;
+        }
+
+        public string Work1()
+        {
+            var result= "Together-1:" + subsystemA.Work1() + subsystemB.Work1();
+            return result;
+        }
+
+        public string Work2()
+        {
+            return "Together-2:" + subsystemA.Work2() + subsystemB.Work2();
+        }
+
+    }
+}
