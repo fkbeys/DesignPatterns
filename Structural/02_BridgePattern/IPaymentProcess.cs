b2bc2253dce4b18a9a30706aa7aa439e65260ae1
fkbeys <fkbeys@gmail.com>
2023-06-09 20:39:03 +0200

    BridgePattern implemented

---
+++ Structural/02_BridgePattern/IPaymentProcess.cs
@@ -0,0 +1,56 @@
+﻿namespace _02_BridgePattern
+{
+    public interface IPaymentProcess
+    {
+        public bool MakePayment(decimal amount);
+    }
+
+    public class Paypal : IPaymentProcess
+    {
+        private readonly string username;
+        private readonly string password;
+
+        public Paypal(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public bool MakePayment(decimal amount)
+        {
+            Console.WriteLine($"Payment succeed with Paypal:{amount} Euro.");
+            return true;
+        }
+    }
+
+
+    public class Stripe : IPaymentProcess
+
+    {
+        public bool MakePayment(decimal amount)
+        {
+            Console.WriteLine($"Payment succeed with Stripe:{amount} Euro.");
+            return true;
+        }
+    }
+
+    public class PaymentProcess
+    {
+        public IPaymentProcess _paymentProcess;
+
+        public PaymentProcess(IPaymentProcess paymentProcess)
+        {
+            _paymentProcess = paymentProcess;
+        }
+
+        public bool MakePayment(decimal amount)
+        {
+            return _paymentProcess.MakePayment(amount);
+        }
+
+
+    }
+
+
+
+}
---
+++ Structural/02_BridgePattern/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02_BridgePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var paypalx = new Paypal("Kaya", "1234");
+
+
+            var proc = new PaymentProcess(paypalx);
+            var result = proc.MakePayment(100);
+
+            if (result == false)
+            {
+                var stripe = new Stripe();
+                proc = new PaymentProcess(stripe);
+                proc.MakePayment(102);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
