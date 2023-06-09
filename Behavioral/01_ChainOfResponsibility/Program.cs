fabe55b2389e7d9de0139bf4b1d1b3f31fae900d
fkbeys <fkbeys@gmail.com>
2023-06-10 01:32:41 +0200

    Chain Of Responsibility pattern implemented

---
+++ Behavioral/01_ChainOfResponsibility/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01_ChainOfResponsibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sale1 = new Sale(100);
+            var sale2 = new Sale(400);
+
+            FivePercentDiscountHandler fivePercentDiscountHandler = new FivePercentDiscountHandler();
+            TenPercentDiscountHandler tenPercentDiscountHandler = new TenPercentDiscountHandler();
+            fivePercentDiscountHandler.SetNextHandler(tenPercentDiscountHandler);
+            fivePercentDiscountHandler.ApplyDiscount(sale1);
+            fivePercentDiscountHandler.ApplyDiscount(sale2);
+
+            
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Behavioral/01_ChainOfResponsibility/Sale.cs
@@ -0,0 +1,66 @@
+﻿namespace _01_ChainOfResponsibility
+{
+    public class Sale
+    {
+        public Sale(int amount)
+        {
+            this.amount = amount;
+            Console.WriteLine("Sale class instance created...");
+        }
+
+        public double amount { get; set; }
+        public double discount { get; set; }
+        public double finalDiscount { get; set; }
+
+    }
+
+    public abstract class DiscountHandler
+    {
+        protected DiscountHandler nextHandler;
+        public void SetNextHandler(DiscountHandler handler)
+        {
+            nextHandler = handler;
+        }
+        public abstract void ApplyDiscount(Sale sale);
+
+    }
+
+    public class FivePercentDiscountHandler : DiscountHandler
+    {
+        public override void ApplyDiscount(Sale sale)
+        {
+            if (sale.amount < 200)
+            {
+                sale.discount = sale.amount * 5 / 100;
+                sale.finalDiscount = sale.amount - sale.discount;
+                Console.WriteLine("Five Percent Disc Hand. Final:" + sale.finalDiscount);
+            }
+            else if (nextHandler != null)
+            {
+                Console.WriteLine("Five Percent Disc Hand. passed the discount to the next handlerer.");
+                nextHandler.ApplyDiscount(sale);
+            }
+        }
+    }
+
+    public class TenPercentDiscountHandler : DiscountHandler
+    {
+        public override void ApplyDiscount(Sale sale)
+        {
+            if (sale.amount >= 200)
+            {
+                sale.discount = sale.amount * 10 / 100;
+                sale.finalDiscount = sale.amount - sale.discount;
+                Console.WriteLine("Ten Percent Disc Hand. Final:" + sale.finalDiscount);
+            }
+            else if (nextHandler != null)
+            {
+                Console.WriteLine("Ten Percent Disc Hand. passed the discount to the next handlerer.");
+                nextHandler.ApplyDiscount(sale);
+            }
+        }
+    }
+
+
+
+}
