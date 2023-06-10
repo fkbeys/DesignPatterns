6adb2e236261b01baab115d2d2656cb2576081a1
fkbeys <fkbeys@gmail.com>
2023-06-10 21:27:35 +0200

    InterpreterPattern

---
+++ Behavioral/04_InterpreterPattern/CustomExpression.cs
@@ -0,0 +1,52 @@
+﻿namespace _04_InterpreterPattern
+{
+    public class CustomContext
+    {
+        //required props etc..
+    }
+
+    public abstract class CustomExpression
+    {
+        public abstract int Interpret(CustomContext context);
+    }
+
+    //terminal expression
+    public class NumberExpression : CustomExpression
+    {
+        public int Number { get; }
+        public NumberExpression(int number)
+        {
+            Number = number;
+        }
+
+        public void addexp()
+        {
+
+        }
+
+        public override int Interpret(CustomContext context)
+        {
+            return Number;
+        }
+    }
+
+    public class AddExpression : CustomExpression
+    {
+        private readonly CustomExpression leftExpression;
+        private readonly CustomExpression righExpression;
+
+        public AddExpression(CustomExpression leftExpression, CustomExpression righExpression)
+        {
+            this.leftExpression = leftExpression;
+            this.righExpression = righExpression;
+        }
+
+
+        public override int Interpret(CustomContext context)
+        {
+            var result = leftExpression.Interpret(context) + righExpression.Interpret(context);
+            return result;
+        }
+    }
+
+}
---
+++ Behavioral/04_InterpreterPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04_InterpreterPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var leftExp = new NumberExpression(2);
+            var rightExp = new NumberExpression(3);
+            var addExp = new AddExpression(leftExp, rightExp);
+            var result = addExp.Interpret(new CustomContext());
+
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
