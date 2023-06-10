5e1eb60b2e1f7d9d1583db165857bcbbb9da22a0
fkbeys <fkbeys@gmail.com>
2023-06-10 22:00:22 +0200

    Medator Pattern implemented

---
+++ Behavioral/05_MediatorPattern/ChattingSystem.cs
@@ -0,0 +1,65 @@
+﻿namespace _05_MediatorPattern
+{
+    public interface IChatRoom
+    {
+        void Register(User user);
+        void SendMessage(string from, string to, string message);
+    }
+    public abstract class User
+    {
+        public string name { get; private set; }
+        public IChatRoom chatRoom { get; set; }
+
+        public User(string name)
+        {
+            this.name = name;
+        }
+
+        public void Send(string to, string message)
+        {
+            chatRoom.SendMessage(name, to, message);
+        }
+
+        public abstract void RecieveMessage(string from, string message);
+
+    }
+
+    public class ChatRoom : IChatRoom
+    {
+        //Dictionary<string, User> userlist = new();
+        List<User> userlist = new();
+        public void Register(User user)
+        {
+            if (!userlist.Contains(user))
+            {
+                userlist.Add(user);
+                user.chatRoom = this;
+            }
+
+        }
+
+        public void SendMessage(string from, string to, string message)
+        {
+            if (userlist.Any(x => x.name == from) && userlist.Any(x => x.name == to))
+            {
+                User? recepientUser = userlist.FirstOrDefault(x => x.name == to);
+                recepientUser?.RecieveMessage(from, message);
+            }
+        }
+    }
+
+    public class Participant : User
+    {
+        public Participant(string name) : base(name)
+        {
+
+        }
+        public override void RecieveMessage(string from, string message)
+        {
+            Console.WriteLine($"From:{from}, To:{name}, Message:{message} ");
+        }
+    }
+
+
+
+}
---
+++ Behavioral/05_MediatorPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05_MediatorPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IChatRoom room = new ChatRoom();
+            User mark=new Participant("Mark");
+            User frank=new Participant("Frank");
+            User george=new Participant("George");
+
+            room.Register(mark);
+            room.Register(frank);
+            room.Register(george);
+
+            room.SendMessage(mark.name, frank.name, "Mark sent message to frank");
+
+
+
+
+            Console.ReadLine();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
