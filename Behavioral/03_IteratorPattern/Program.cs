072413426718d174855c48e7cd3c31177342b4f0
fkbeys <fkbeys@gmail.com>
2023-06-10 20:53:44 +0200

    IteratorPattern implemented

---
+++ Behavioral/03_IteratorPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace _03_IteratorPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StudentCollection studentCollection = new StudentCollection();
+            Student student = new Student("Mark", "Twain");
+            studentCollection.AddStudent(student);
+            IEnumerator enumerator = studentCollection.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Student stnd = (Student)enumerator.Current;
+                Console.WriteLine(stnd.name);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Behavioral/03_IteratorPattern/Student.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace _03_IteratorPattern
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+
+        public Student(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+
+    public class StudentCollection : IEnumerable
+    {
+        private ArrayList _students = new();
+
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _students.GetEnumerator();
+        }
+
+
+
+    }
+}
