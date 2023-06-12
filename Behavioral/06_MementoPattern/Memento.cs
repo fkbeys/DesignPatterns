26d83fd1ff063892d211a25dbc28d18509f72546
fkbeys <fkbeys@gmail.com>
2023-06-12 11:35:02 +0200

    Medator and Memento patterns implementations

---
+++ Behavioral/06_MementoPattern/Memento.cs
@@ -0,0 +1,45 @@
+﻿namespace _06_MementoPattern
+{
+    public class Memento
+    {
+        public string State { get; }
+        public Memento(string state)
+        {
+            State = state;
+        }
+        public string GetState() { return State; }
+    }
+
+    public class Originator
+    {
+        private string _state;
+        public void SetState(string state)
+        {
+            _state = state;
+            Console.WriteLine($"Current State:{_state}");
+        }
+        public Memento SaveState()
+        {
+            return new Memento(_state);
+        }
+        public void RestoreState(Memento memento)
+        {
+            _state = memento.GetState();
+            Console.WriteLine("Latest state rolled back:" + _state);
+        }
+    }
+
+    public class CareTaker
+    {
+        private Memento _memento;
+        public void Save(Originator originator)
+        {
+            _memento = originator.SaveState();
+        }
+        public void RollBackState(Originator originator)
+        {
+            originator.RestoreState(_memento);
+        }
+    }
+
+}
---
+++ Behavioral/06_MementoPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06_MementoPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Originator originator = new();
+            CareTaker careTaker = new();
+
+            originator.SetState("Insert into 2 tables at the same time in one transaction");
+            careTaker.Save(originator);
+
+            originator.SetState("Cancel the transaction due to an error");
+
+            careTaker.RollBackState(originator);
+
+            Console.ReadLine();
+        }
+    }
+}
