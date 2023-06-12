7bc44edc07e66e993ac55c26b681d2885731f4da
fkbeys <fkbeys@gmail.com>
2023-06-12 16:40:08 +0200

    Template Method implemented

---
+++ Behavioral/10_TemplateMethodPattern/DrinkClass.cs
@@ -0,0 +1,56 @@
+﻿namespace _10_TemplateMethodPattern
+{
+    public abstract class Drink
+    {
+        public void PrepareDrink()
+        {
+            BoilTheWather();
+            Brew();
+            FillTheCup();
+            Serve();
+        }
+
+        private void BoilTheWather()
+        {
+            Console.WriteLine("Water boiled");
+        }
+        public abstract void Brew();
+        private void FillTheCup()
+        {
+            Console.WriteLine("Filling the cup.");
+        }
+        public abstract void Serve();
+
+    }
+
+    public class Tea : Drink
+    {
+        public override void Brew()
+        {
+            Console.WriteLine("Tea brewed");
+        }
+
+
+        public override void Serve()
+        {
+            Console.WriteLine("Tea served");
+        }
+    }
+
+    public class Coffea : Drink
+    {
+        public override void Brew()
+        {
+            Console.WriteLine("Coffea brewed");
+        }
+
+
+        public override void Serve()
+        {
+            Console.WriteLine("Coffea served.");
+        }
+    }
+
+
+
+}
---
+++ Behavioral/10_TemplateMethodPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _10_TemplateMethodPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Drink drnk = new Tea();
+            drnk.PrepareDrink();
+
+            drnk = new Coffea();
+            drnk.PrepareDrink();
+
+            Console.ReadLine();
+        }
+    }
+}
