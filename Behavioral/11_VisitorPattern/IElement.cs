f7010c0743e4297f2373593cfc54a3417d6ff589
fkbeys <fkbeys@gmail.com>
2023-06-12 16:55:31 +0200

    Visitor Pattern Implemented

---
+++ Behavioral/11_VisitorPattern/IElement.cs
@@ -0,0 +1,51 @@
+﻿namespace _11_VisitorPattern
+{
+    public interface IVizitor
+    {
+        public void Visit(ConcreteA concreteA);
+        public void Visit(ConcreteB concreteB);
+    }
+
+    public interface IElement
+    {
+        public void Accept(IVizitor vizitor);
+
+    }
+
+    public class ConcreteA : IElement
+    {
+        public void Accept(IVizitor vizitor)
+        {
+            vizitor.Visit(this);
+        }
+        public string OperationA()
+        {
+            return "Operation A";
+        }
+    }
+
+    public class ConcreteB : IElement
+    {
+        public void Accept(IVizitor vizitor)
+        {
+            vizitor.Visit(this);
+        }
+        public string OperationB()
+        {
+            return "Operation B";
+        }
+    }
+
+    public class ConcreteVisiter : IVizitor
+    {
+        public void Visit(ConcreteA concreteA)
+        {
+            Console.WriteLine("Visitir A operation A" + concreteA.OperationA());
+        }
+
+        public void Visit(ConcreteB concreteB)
+        {
+            Console.WriteLine("Visitir B operation B:" + concreteB.OperationB());
+        }
+    }
+}
---
+++ Behavioral/11_VisitorPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _11_VisitorPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ConcreteVisiter concreteVisiter = new ConcreteVisiter();
+            //concreteVisiter.Visit(new ConcreteA());
+            //concreteVisiter.Visit(new ConcreteB());
+
+            var elemnets = new IElement[] { new ConcreteA(), new ConcreteB() };
+            var vizitor = new ConcreteVisiter();
+
+            foreach (var element in elemnets)
+            {
+                element.Accept(vizitor);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
