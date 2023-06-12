50b35d1c2dbb7bdfc8d2d0b77348228e652e13a6
fkbeys <fkbeys@gmail.com>
2023-06-12 16:30:33 +0200

    Strategy Pattern implemented

---
+++ Behavioral/09_StrategyPattern/DrawClass.cs
@@ -0,0 +1,55 @@
+﻿namespace _09_StrategyPattern
+{
+    public interface IDrawStrategy
+    {
+        public void Draw();
+
+    }
+
+    public class DrawCircle : IDrawStrategy
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Circle is drown");
+        }
+    }
+
+    public class DrawRectangle : IDrawStrategy
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Rectangle is drown");
+        }
+    }
+    public class TriangleRectangle : IDrawStrategy
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Triangle is drown");
+        }
+    }
+
+
+    public class Shape
+    {
+        private IDrawStrategy _drawStrategy;
+
+        public Shape(IDrawStrategy drawStrategy)
+        {
+            this._drawStrategy = drawStrategy;
+        }
+
+        public void SetDrawStrategy(IDrawStrategy drawStrategy)
+        {
+            _drawStrategy = drawStrategy;
+        }
+
+        public void Drow()
+        {
+            _drawStrategy.Draw();
+        }
+
+    }
+
+
+}
---
+++ Behavioral/09_StrategyPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _09_StrategyPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Shape shp = new Shape(new DrawCircle());
+            shp.Drow();
+            shp.Drow();
+            shp.SetDrawStrategy(new DrawRectangle());
+            shp.Drow();
+            shp.SetDrawStrategy(new TriangleRectangle());
+            shp.Drow();
+
+
+
+            Console.Read();
+        }
+    }
+}
