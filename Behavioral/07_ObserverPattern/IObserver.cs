5d4b705e3716f70e7da065f6ea21e657bf3abdd0
fkbeys <fkbeys@gmail.com>
2023-06-12 13:05:25 +0200

    ObserverPattern implemented

---
+++ Behavioral/07_ObserverPattern/IObserver.cs
@@ -0,0 +1,57 @@
+﻿namespace _07_ObserverPattern
+{
+    public interface IObserver
+    {
+        public void Update(string message);
+    }
+
+    public interface IObservable
+    {
+        public void Register(IObserver observer);
+        public void UnRegister(IObserver observer);
+        public void Notify();
+    }
+    public class Observable : IObservable
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+        private string _message;
+        public void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(_message);
+            }
+        }
+
+        public void Register(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void UnRegister(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+        public void SendMessage(string message)
+        {
+            _message = message;
+            Notify();
+        }
+    }
+
+    public class MobilePhone : IObserver
+    {
+        private string Model { get; }
+        public MobilePhone(string model)
+        {
+            Model = model;
+        }
+
+
+        public void Update(string message)
+        {
+            Console.WriteLine(Model+" message received:" + message);
+        }
+    }
+
+}
---
+++ Behavioral/07_ObserverPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _07_ObserverPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Observable observable = new Observable();
+            observable.Register(new MobilePhone("Samsung"));
+            observable.Register(new MobilePhone("Xiaomi"));
+            observable.Register(new MobilePhone("Apple"));
+
+            observable.Notify();
+
+            Console.ReadLine();
+        }
+    }
+}
